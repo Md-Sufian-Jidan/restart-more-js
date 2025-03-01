c72d84c838ec51ea52e348872518a4f2ac2f7ae2
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-01 17:24:56 +0600

    fetch and promise function added

--- async.js
+++ async.js
@@ -1,12 +1,37 @@
-console.log(1);
-console.log(2);
-setTimeout(() => {
-    console.log('lazy logged');
-}, 4000);
-console.log(4);
-console.log(5);
-console.log(6);
 
-function doSomething() {
-    console.log(3);
+const myLoader = () => {
+    return new Promise((resolve, reject) => {
+        const success = true;
+        if (success) {
+            resolve(0.3)
+        }
+        else {
+            reject(0.7)
+        }
+    })
 }
+
+myLoader()
+    .then(data => console.log('resolved data', data))
+    .catch(err => console.log('rejected with value', err))
+
+fetch()
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+
+async function loadData() {
+    const res = await fetch('');
+    const data = await res.json();
+    console.log(data);
+}
+
+
+const taskLoader = async () => {
+    const res = await res.json();
+    const data = await res;
+    console.log(data);
+}
+taskLoader();
+loadData()
